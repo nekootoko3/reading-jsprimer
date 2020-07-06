cd4aa3c18d56dd3efb0e047ecffe50ee4f1274b5
nekootoko3 <miyako.kerria@gmail.com>
2020-07-06 13:07:20 +0900

    Impl fetchUserInfo

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -1,1 +1,18 @@
 console.log("index.js: loaded");
+
+const fetchUserInfo = (userId) => {
+  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    .then((res) => {
+      console.log(res.status);
+      if (!res.ok) {
+        console.error("error response", res.status);
+      } else {
+        return res.json().then((userInfo) => {
+          console.log(userInfo);
+        });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
