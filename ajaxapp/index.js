321dfbaa40802c00a3f2f0726fae237cb8fa43d2
nekootoko3 <miyako.kerria@gmail.com>
2020-07-06 17:34:21 +0900

    Show userInfo on click button

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -3,16 +3,46 @@ console.log("index.js: loaded");
 const fetchUserInfo = (userId) => {
   fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
     .then((res) => {
-      console.log(res.status);
       if (!res.ok) {
         console.error("error response", res.status);
       } else {
         return res.json().then((userInfo) => {
-          console.log(userInfo);
+          const view = escapeHTML`
+          <h4>${userInfo.name} (@${userInfo.login})</h4>
+          <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+          <dl>
+              <dt>Location</dt>
+              <dd>${userInfo.location}</dd>
+              <dt>Repositories</dt>
+              <dd>${userInfo.public_repos}</dd>
+          </dl>
+          `;
+          const result = document.getElementById("result");
+          result.innerHTML = view;
         });
       }
     })
     .catch((error) => {
       console.error(error);
     });
+};
+
+const escapeSpecialChars = (str) => {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+};
+
+const escapeHTML = (strings, ...values) => {
+  return strings.reduce((result, str, i) => {
+    const value = values[i - 1];
+    if (typeof value === "string") {
+      return result + escapeSpecialChars(value) + str;
+    } else {
+      return result + String(value) + str;
+    }
+  });
 };
