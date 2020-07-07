6b3df07069af861cc619af0d804ecf56393cbacd
nekootoko3 <miyako.kerria@gmail.com>
2020-07-07 09:01:43 +0900

    Enable to read file

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,7 +1,15 @@
 const program = require("commander");
+const fs = require("fs");
 
 program.parse(process.argv);
-
 const filePath = program.args[0];
-console.log(program.args);
-console.log(filePath);
+
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+  if (err) {
+    console.error(err.message);
+    process.exit(1);
+    return;
+  }
+
+  console.log(file);
+});
