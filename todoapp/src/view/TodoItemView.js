c7da9784290c69248b2e6b3e34e8e5d5919df817
nekootoko3 <miyako.kerria@gmail.com>
2020-07-09 07:26:21 +0900

    Create TodoItemView

---
+++ todoapp/src/view/TodoItemView.js
@@ -0,0 +1,27 @@
+import { element } from "./html-util.js";
+
+export class TodoItemView {
+  createElement(todoItem, { onUpdateTodo, onDeleteTodo }) {
+    const todoItemElement = todoItem.completed
+      ? element`<li><input type="checkbox" class="checkbox" checked>
+            <s>${item.title}</s>
+            <button class="delete">x</button>
+          </li>`
+      : element`<li><input type="checkbox" class="checkbox">
+            ${item.title}
+            <button class="delete">x</button>
+          </li>`;
+    const inputCheckboxElement = todoItemElement.querySelector(".checkbox");
+    inputCheckboxElement.addEventListener("change", () => {
+      onUpdateTodo({
+        id: item.id,
+        completed: !item.completed,
+      });
+    });
+    const deleteButtonElement = todoItemElement.querySelector(".delete");
+    deleteButtonElement.addEventListener("click", () => {
+      onDeleteTodo({ id: item.id });
+    });
+    return todoItemElement;
+  }
+}
