ed16b1a6bb72aa9bdd51f271208326ae855d740c
nekootoko3 <miyako.kerria@gmail.com>
2020-07-08 12:02:30 +0900

    Create TodoListModel

---
+++ todoapp/src/model/TodoListModel.js
@@ -0,0 +1,29 @@
+import { EventEmitter } from "../EventEmitter.js";
+
+export class TodoListModel extends EventEmitter {
+  constructor() {
+    super();
+    this.items = [];
+  }
+
+  getTotalCount() {
+    return this.items.length();
+  }
+
+  getTodoItems() {
+    return this.items;
+  }
+
+  onChange(listener) {
+    this.addEventListener("change", listener);
+  }
+
+  emitChange() {
+    this.emit("change");
+  }
+
+  addTodo(todoItem) {
+    this.items.push(todoItem);
+    this.emitChange();
+  }
+}
